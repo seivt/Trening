673717714fe10a62e9f9f115591870d16537f962
My NameSergey <vrxslk@yandex.ru>
2022-07-24 23:28:29 +0600

    Methods

--- Main.java
+++ Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void method() {
+        System.out.println("Test");
+    }
+}
+
+class Print {
+    private int el = 1;
+
+    public void printEl() {
+        System.out.println(el);
+    }
 }
